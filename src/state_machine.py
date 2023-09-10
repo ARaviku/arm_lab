b041ed3b19d5c02e037c78701ddd54d22145dadc
anurekha <anurekharavikumar@gmail.com>
2023-09-09 23:28:30 -0400

    armlab project

---
+++ src/state_machine.py
@@ -0,0 +1,275 @@
+"""!
+The state machine that implements the logic.
+"""
+from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot, QTimer
+import time
+import numpy as np
+import rclpy
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+
+matplotlib.use('Agg')
+
+plt.rcParams["figure.figsize"] = (8.0, 8.0)
+plt.rcParams['font.size'] = 12
+plt.rcParams['image.interpolation'] = "nearest"
+
+class StateMachine():
+    """!
+    @brief      This class describes a state machine.
+
+                TODO: Add states and state functions to this class to implement all of the required logic for the armlab
+    """
+
+    def __init__(self, rxarm, camera):
+        """!
+        @brief      Constructs a new instance.
+
+        @param      rxarm   The rxarm
+        @param      planner  The planner
+        @param      camera   The camera
+        """
+        self.rxarm = rxarm
+        self.camera = camera
+        self.status_message = "State: Idle"
+        self.current_state = "idle"
+        self.next_state = "idle"
+        self.waypoints = [
+            [-np.pi/2,       -0.5,      -0.3,          0.0,        0.0],
+            [0.75*-np.pi/2,   0.5,       0.3,     -np.pi/3,    np.pi/2],
+            [0.5*-np.pi/2,   -0.5,      -0.3,      np.pi/2,        0.0],
+            [0.25*-np.pi/2,   0.5,       0.3,     -np.pi/3,    np.pi/2],
+            [0.0,             0.0,       0.0,          0.0,        0.0],
+            [0.25*np.pi/2,   -0.5,      -0.3,          0.0,    np.pi/2],
+            [0.5*np.pi/2,     0.5,       0.3,     -np.pi/3,        0.0],
+            [0.75*np.pi/2,   -0.5,      -0.3,          0.0,    np.pi/2],
+            [np.pi/2,         0.5,       0.3,     -np.pi/3,        0.0],
+            [0.0,             0.0,       0.0,          0.0,        0.0]]
+
+        self.joint_pos = []
+        self.grasp_pos = []
+        self.release_pos = []
+
+    def set_next_state(self, state):
+        """!
+        @brief      Sets the next state.
+
+            This is in a different thread than run so we do nothing here and let run handle it on the next iteration.
+
+        @param      state  a string representing the next state.
+        """
+        self.next_state = state
+
+    def run(self):
+        """!
+        @brief      Run the logic for the next state
+
+                    This is run in its own thread.
+
+                    TODO: Add states and funcitons as needed.
+        """
+        if self.next_state == "initialize_rxarm":
+            self.initialize_rxarm()
+
+        if self.next_state == "idle":
+            self.idle()
+
+        if self.next_state == "estop":
+            self.estop()
+
+        if self.next_state == "execute":
+            self.execute()
+
+        if self.next_state == "calibrate":
+            self.calibrate()
+
+        if self.next_state == "detect":
+            self.detect()
+
+        if self.next_state == "record":
+            self.record()
+
+        if self.next_state == "grasp":
+            self.grip_grasp()
+
+        if self.next_state == "release":
+            self.grip_release()
+        
+        if self.next_state == "save":
+            self.save()
+        
+        if self.next_state == "reset":
+            self.reset()
+
+        if self.next_state == "manual":
+            self.manual()
+
+
+    """Functions run for each state"""
+
+    def manual(self):
+        """!
+        @brief      Manually control the rxarm
+        """
+        self.status_message = "State: Manual - Use sliders to control arm"
+        self.current_state = "manual"
+
+    def idle(self):
+        """!
+        @brief      Do nothing
+        """
+        self.status_message = "State: Idle - Waiting for input"
+        self.current_state = "idle"
+
+    def estop(self):
+        """!
+        @brief      Emergency stop disable torque.
+        """
+        self.status_message = "EMERGENCY STOP - Check rxarm and restart program"
+        self.current_state = "estop"
+        self.rxarm.disable_torque()
+
+    def execute(self):
+        """!
+        @brief      Go through all waypoints
+        TODO: Implement this function to execute a waypoint plan
+              Make sure you respect estop signal
+        """
+        self.status_message = "State: Execute - Executing motion plan"
+        for i in range(len(self.joint_pos)):
+            if i in self.grasp_pos:
+                self.rxarm.gripper.grasp()
+                time.sleep(1)
+
+            if i in self.release_pos:
+                self.rxarm.gripper.release()
+                time.sleep(1)
+
+            self.rxarm.set_positions(self.joint_pos[i])
+            time.sleep(0.8)
+
+        self.next_state = "idle"
+
+    def calibrate(self):
+        """!
+        @brief      Gets the user input to perform the calibration
+        """
+        self.current_state = "calibrate"
+        self.next_state = "idle"
+
+        """TODO Perform camera calibration routine here"""
+        self.status_message = "Calibration - Completed Calibration"
+
+    """ TODO """
+    def detect(self):
+        """!
+        @brief      Detect the blocks
+        """
+        time.sleep(1)
+
+    def record(self):
+        """
+        @brief Record joint positions and angles
+        """
+        # self.current_state = "record"
+        self.joint_pos.append(self.rxarm.get_positions())
+        # print(f"The position {self.joint_pos}")
+        self.next_state = "idle"
+    
+    def grip_grasp(self):
+        """!
+        @brief      Records the points at which grip has to be grasped.      
+        """
+        # self.current_state = "grasp"
+        self.grasp_pos.append(len(self.joint_pos))
+        self.next_state = 'idle'
+
+    def grip_release(self):
+        """!
+        @brief      Records the points at which grip is released.
+        """
+        # self.current_state = "release"
+        self.release_pos.append(len(self.joint_pos))
+        self.next_state = 'idle'
+
+    def save(self):
+        """!
+        @brief  Saves the current position
+        """
+
+        df = pd.DataFrame(self.joint_pos)
+        csv_file = df.to_csv('/home/student_pm/armlab-f23/csv_files/joint_pos.csv', index=True)
+
+        df_grip_close = pd.DataFrame(self.grasp_pos)
+        csv_file = df_grip_close.to_csv('/home/student_pm/armlab-f23/csv_files/close_grip.csv', index=True)
+
+        df_grip_open = pd.DataFrame(self.release_pos)
+        csv_file = df_grip_open.to_csv('/home/student_pm/armlab-f23/csv_files/open_grip.csv', index=True)
+        time.sleep(2)
+        
+        self.next_state = 'idle'
+
+    def reset(self):
+        """!
+        @brief Reset joint_pos list, grasp_position and release position
+        """
+
+        self.joint_pos = []
+        self.grasp_pos = []
+        self.release_pos = []
+        self.next_state = 'idle'
+
+    def initialize_rxarm(self):
+        """!
+        @brief      Initializes the rxarm.
+        """
+        self.current_state = "initialize_rxarm"
+        self.status_message = "RXArm Initialized!"
+        if not self.rxarm.initialize():
+            print('Failed to initialize the rxarm')
+            self.status_message = "State: Failed to initialize the rxarm!"
+            time.sleep(5)
+        self.next_state = "idle"
+
+class StateMachineThread(QThread):
+    """!
+    @brief      Runs the state machine
+    """
+    updateStatusMessage = pyqtSignal(str)
+    
+    def __init__(self, state_machine, parent=None):
+        """!
+        @brief      Constructs a new instance.
+
+        @param      state_machine  The state machine
+        @param      parent         The parent
+        """
+        QThread.__init__(self, parent=parent)
+        self.sm=state_machine
+
+    def run(self):
+        """!
+        @brief      Update the state machine at a set rate
+        """
+        while True:
+            self.sm.run()
+            self.updateStatusMessage.emit(self.sm.status_message)
+            time.sleep(0.05)
+
+
+def plot_joints(joint_pos:list=None):
+    """
+    Plots and saves the joint positions for Task 1.3
+    """
+    os.makedirs("plots", exist_ok=True)
+    path = os.path.join("plots")
+    
+    plt.plot(joint_pos[0])
+    plt.plot(joint_pos[1])
+    plt_plot(joint_pos[2])
+    plt.xlabel("X")
+    plt.ylabel("Y")
+
